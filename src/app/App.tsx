e670e287956023177890d40a6a9b7b35d3774381
Alona Borealis <nadiadmitrov@gmail.com>
2026-01-21 20:26:06 +0300

    добавила возможность переключать тему

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,98 @@
-import { useState } from "react"
-import reactLogo from "../assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import { useState } from "react";
+import Button from "@mui/material/Button";
 
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import AppBar from "./AppBar.tsx";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+	Box,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	Radio,
+	RadioGroup,
+	useColorScheme,
+} from "@mui/material";
+
+const theme = createTheme({
+	colorSchemes: {
+		dark: true,
+	},
+});
 function App() {
-	const [count, setCount] = useState(0)
-
+	const [count, setCount] = useState(0);
+	const { mode, setMode } = useColorScheme();
+	if (!mode) {
+		return null;
+	}
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<AppBar />
+				<div style={{ marginTop: "100px" }}>
+					<a href="https://vite.dev" target="_blank" rel="noreferrer">
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</a>
+					<a href="https://react.dev" target="_blank" rel="noreferrer">
+						<img src={reactLogo} className="logo react" alt="React logo" />
+					</a>
+				</div>
+				<h1>Vite + React</h1>
+				<div className="card">
+					<Button variant="contained" onClick={() => setCount((count) => count + 1)}>
+						count is {count}
+					</Button>
+					<p>
+						Edit <code>src/App.tsx</code> and save to test HMR
+					</p>
+				</div>
+				<p className="read-the-docs">
+					Click on the Vite and React logos to learn more
 				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+				<Box
+					sx={{
+						display: "flex",
+						width: "100%",
+						alignItems: "center",
+						justifyContent: "center",
+						bgcolor: "background.default",
+						color: "text.primary",
+						borderRadius: 1,
+						p: 3,
+						minHeight: "56px",
+					}}
+				>
+					<FormControl>
+						<FormLabel id="demo-theme-toggle">Theme</FormLabel>
+						<RadioGroup
+							aria-labelledby="demo-theme-toggle"
+							name="theme-toggle"
+							row
+							value={mode}
+							onChange={(event) =>
+								setMode(event.target.value as "light" | "dark")
+							}
+						>
+							<FormControlLabel
+								value="light"
+								control={<Radio />}
+								label="Light"
+							/>
+							<FormControlLabel value="dark" control={<Radio />} label="Dark" />
+						</RadioGroup>
+					</FormControl>
+				</Box>
+			</ThemeProvider>
 		</>
-	)
+	);
 }
 
-export default App
+export default App;
